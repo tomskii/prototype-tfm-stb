130e828631d15f249fddd4f86d5fea60420eb9f6
dap-protos <108474367+tom-decs@users.noreply.github.com>
2023-04-28 16:50:11 +0100

    Initial commit

---
+++ app/data/session-data-defaults.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+ // Insert values here
+  
+  // Dates
+  'report-due': 'yes',
+  'end-previous-month': '28 February 2023',
+  'end-current-month': '31 March 2023',
+  'last-month': 'February 2023',
+  'report-month': 'March 2023',
+  '1-month-ago': '3 March 2023',
+  'todays-date': '28 April 2023',
+  'todays-date-full': '28 April 2023',
+  'this-month-report-due': '18 April 2023',
+  'next-month-report-due': '15 May 2023',
+  
+  // Names
+  'signed-in-name': 'John Smith',
+  'last-month-name': 'John Smith',
+
+  // FACILITY 1 VALUES
+  'facility-1-updated': 'no',
+  'facility-1-name': 'Compressors Ltd',
+  'facility-1-ukef-id': '0020001371',
+  'facility-1-ref': '0239397',
+  'facility-1-start-date': '16 June 2022',
+  'facility-1-end-date': '16 June 2024',
+  'facility-1-tenor': '36 months',
+
+  'facility-1-interest-margin': '2%',
+  'facility-1-fee-payable': '1.8000%',
+  'facility-1-credit-rating': 'Good (BB-)',
+
+  'facility-1-prob-default': '1.0%',
+  'facility-1-limit': '600,000.00',
+  'facility-1-cover': '80%',
+  'facility-1-max': '480,000.00',
+  'facility-1-utilisation': '400,000.00',
+  'facility-1-exposure': '320,000.00',
+  'facility-1-paid': '1,000.00',
+  'facility-1-accrued': '10,000.00',
+  'facility-1new-utilisation': '',
+  'facility-1new-exposure': '',
+  'facility-1new-paid': '',
+  'facility-1new-accrued': '',
+  'facility-1utiisation-updated': 'no',
+  'facility-1paid-updated': 'no',
+  'facility-1accrued-updated': 'no',
+}
---
+++ app/routes.js
@@ -0,0 +1,20 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+// Logs saved data in the terminal
+router.use((req, res, next) => {
+    const log = {
+      method: req.method,
+      url: req.originalUrl,
+      data: req.session.data
+    }
+    console.log(JSON.stringify(log, null, 2))
+  next()
+})
